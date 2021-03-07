37ce83f29e93df355ed320f69ca689c802a1ef9e
Ankit Pant <ankit.pant@klm.com>
2021-03-07 20:48:16 +0100

    added some code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Testing the git hooks");
+class Animals {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public getName() {
+    return this.name;
+  }
+
+  public setName(name: string) {
+    this.name = name;
+  }
+}
+
+class Dog extends Animals {
+  constructor() {
+    super("doggy");
+  }
+}
+
+const nameOfTheAnimal = new Dog().getName();
+
+console.log("The name of the animal is: " + nameOfTheAnimal);
